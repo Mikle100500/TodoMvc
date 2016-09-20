c8489bbda2720c1488828968873d5612b9a293c6
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-09-20 15:03:24 +0300

    add .gitignore

--- src/test/java/com/todomvc/test/TodoMVCTest.java
+++ src/test/java/com/todomvc/test/TodoMVCTest.java
@@ -3,8 +3,8 @@ package com.todomvc.test;
 import com.todomvc.pages.TaskManagerPage;
 import org.junit.Test;
 
-import static com.codeborne.selenide.CollectionCondition.*;
-import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.CollectionCondition.empty;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Selenide.open;
 
 
