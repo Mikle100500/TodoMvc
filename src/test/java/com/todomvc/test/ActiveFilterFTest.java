7e11e1213d63cf52743f4419416fb9edcd63b461
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-10-11 14:54:35 +0300

    ready to review

--- src/test/java/com/todomvc/test/ActiveFilterFTest.java
+++ src/test/java/com/todomvc/test/ActiveFilterFTest.java
@@ -1,5 +1,99 @@
 package com.todomvc.test;
 
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.todomvc.helpers.TaskBuilder.build;
+import static com.todomvc.helpers.TaskStatus.ACTIVE;
+import static com.todomvc.helpers.TaskStatus.COMPLETED;
+
 public class ActiveFilterFTest extends BaseTest {
 
+    @Test
+    public void testEdit(){
+
+        page.given(build("a", ACTIVE));
+        page.filterActive();
+
+        page.startEdit("a", "a edited").pressEnter();
+        page.assertVisibleTasks("a edited");
+    }
+
+    @Test
+    public void testDelete(){
+
+        page.given(build(ACTIVE, "a", "b"));
+        page.filterActive();
+
+        page.delete("a");
+        page.assertVisibleTasks("b");
+
+    }
+
+    @Test
+    public void testComplete(){
+
+        page.given(build(ACTIVE, "a", "b"));
+        page.filterActive();
+
+        page.toggle("a");
+        page.assertVisibleTasks("b");
+        page.assertItemsLeft(1);
+
+    }
+
+    @Test
+    public void testActivateAll(){
+
+        page.given(build(ACTIVE, "a", "b", "c", "d"));
+        page.filterActive();
+
+        page.toggleAll();
+        page.assertNoVisibleTasks();
+
+    }
+
+
+    @Test
+    public void testClearCompleted(){
+
+        page.given(build(COMPLETED, "a", "b", "c", "d"));
+        page.filterActive();
+
+        page.clearCompleted();
+        page.assertNoVisibleTasks();
+
+    }
+
+    @Test
+    public void testEditWithTab(){
+
+        page.given(build(ACTIVE, "a"));
+        page.filterActive();
+
+        page.startEdit("a", "a edited").pressTab();
+        page.assertVisibleTasks("a edited");
+    }
+
+    @Test
+    public void testEditWithClick(){
+
+        page.given(build(ACTIVE, "a"));
+        page.filterActive();
+
+        page.startEdit("a", "a edited");
+        $("#header").click();
+        page.assertVisibleTasks("a edited");
+    }
+
+
+    @Test
+    public void testDeleteWithEmptying(){
+
+        page.given(build(ACTIVE, "a"));
+        page.filterActive();
+
+        page.startEdit("a", "").pressEnter();
+        page.assertNoVisibleTasks();
+    }
 }
--- src/test/java/com/todomvc/test/CompletedFilterFTest.java
+++ src/test/java/com/todomvc/test/CompletedFilterFTest.java
@@ -1,8 +1,96 @@
 package com.todomvc.test;
 
-/**
- * Created by m.pavlenko on 10/10/2016.
- */
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.todomvc.helpers.TaskBuilder.build;
+import static com.todomvc.helpers.TaskStatus.ACTIVE;
+import static com.todomvc.helpers.TaskStatus.COMPLETED;
+
 public class CompletedFilterFTest extends BaseTest {
-    // TODO: 10/10/2016
+
+    @Test
+    public void testCreate(){
+
+        page.given(build("a", ACTIVE));
+        page.filterCompleted();
+
+        page.create("b");
+        page.assertItemsLeft(2);
+        page.assertNoVisibleTasks();
+    }
+
+    @Test
+    public void testEdit(){
+
+        page.given(build("a", COMPLETED));
+        page.filterCompleted();
+
+        page.startEdit("a", "a edited").pressEnter();
+        page.assertVisibleTasks("a edited");
+    }
+
+    @Test
+    public void testDelete(){
+
+        page.given(build(COMPLETED, "a", "b", "c"));
+        page.filterCompleted();
+
+        page.delete("a");
+        page.assertVisibleTasks("b", "c");
+    }
+
+    @Test
+    public void testCompleteAll(){
+
+        page.given(build(ACTIVE, "a", "b", "c"));
+        page.filterCompleted();
+
+        page.toggleAll();
+        page.assertVisibleTasks("a", "b", "c");
+    }
+
+    @Test
+    public void testCancelEditWithEsc(){
+
+        page.given(build(COMPLETED, "a"));
+        page.filterCompleted();
+
+        page.startEdit("a", "a edited").pressEscape();
+        page.assertVisibleTasks("a");
+    }
+
+    @Test
+    public void testEditWithTab(){
+
+        page.given(build(COMPLETED, "a"));
+        page.filterCompleted();
+
+        page.startEdit("a", "a edited").pressTab();
+        page.assertVisibleTasks("a edited");
+    }
+
+    @Test
+    public void testEditWithClick(){
+
+        page.given(build(COMPLETED, "a"));
+        page.filterCompleted();
+
+        page.startEdit("a", "a edited");
+        $("#header").click();
+        page.assertVisibleTasks("a edited");
+    }
+
+
+    @Test
+    public void testDeleteWithEmptying(){
+
+        page.given(build(COMPLETED, "a"));
+        page.filterCompleted();
+
+        page.startEdit("a", "").pressEnter();
+        page.assertNoVisibleTasks();
+    }
+
+
 }
