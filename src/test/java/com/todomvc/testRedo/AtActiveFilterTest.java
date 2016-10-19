cded3e51de3ad3ecf43bd01811574b8f4b3fd9fb
Mikle100500 <mikhail.pavlenko84@gmail.com>
2016-10-19 17:58:29 +0300

    move to filter builder redo

--- src/test/java/com/todomvc/testRedo/AtActiveFilterTest.java
+++ src/test/java/com/todomvc/testRedo/AtActiveFilterTest.java
@@ -1,11 +1,53 @@
 package com.todomvc.testRedo;
 
 import com.todomvc.pages.TaskManagerPage;
+import org.junit.Test;
 
-/**
- * Created by m.pavlenko on 19/10/2016.
- */
+import static com.todomvc.builder.Task.given;
+
 public class AtActiveFilterTest {
 
     private TaskManagerPage page = new TaskManagerPage();
+
+    @Test
+    public void testCreate(){
+
+        given().atActiveFilter().build();
+
+        page.create("a");
+        page.assertVisibleTasks("a");
+        page.assertItemsLeft(1);
+    }
+
+    @Test
+    public void testEdit(){
+
+        given().activeTasks("a").atActiveFilter().build();
+
+        page.startEdit("a", "a edited").pressEnter();
+        page.assertVisibleTasks("a edited");
+        page.assertItemsLeft(1);
+    }
+
+    @Test
+    public void testDelete(){
+
+        given().activeTasks("a", "b").atActiveFilter().build();
+
+        page.delete("a");
+        page.assertVisibleTasks("b");
+        page.assertItemsLeft(1);
+    }
+
+    @Test
+    public void testComplete(){
+
+        given().activeTasks("a", "b").atActiveFilter().build();
+
+        page.toggle("a");
+        page.assertVisibleTasks("b");
+        page.assertItemsLeft(1);
+    }
+
+
 }
